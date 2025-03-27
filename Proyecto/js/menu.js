9245de5377a3ee271e81a133da59ee154dcfc2a1
Celtia <celtiacastelo@gmail.com>
2025-03-27 16:01:14 +0100

    menu burger

---
+++ Proyecto/js/menu.js
@@ -0,0 +1,23 @@
+document.getElementById("menuToggle").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que recargue la página
+    const nav = document.getElementById("NavPrincipal");
+
+    // Si el menú está visible, lo ocultamos, y si está oculto, lo mostramos
+  
+        if (nav.style.display === "none") {
+            nav.style.display = "block";
+        } else {
+            nav.style.display = "none";
+       }
+    
+});
+
+// Asegúrate de que el menú esté siempre visible en pantallas grandes
+window.addEventListener("resize", function() {
+    const nav = document.getElementById("NavPrincipal");
+    if (window.innerWidth > 769) {
+        nav.style.display = "block";  // El menú debe estar siempre visible en pantallas grandes
+    } else {
+        nav.style.display = "none";   // El menú se oculta en pantallas pequeñas por defecto
+    }
+});
